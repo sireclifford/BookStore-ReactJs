da19f7db635d07c9fb363a414be93f89fd0d9d75
sireclifford <dev.sire.clifford@gmail.com>
2022-08-15 21:24:39 +0000

    uninstalled react-bootstrap library

--- src/components/addBook/AddBook.js
+++ src/components/addBook/AddBook.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './AddBook.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 
 const AddBook = () => (
   <div className="add-book">
@@ -9,23 +7,23 @@ const AddBook = () => (
       <div className="heading">
         <h1>Add New Book</h1>
       </div>
-      <Form className="add-book-form">
-        <Form.Group className="title mb-3 mr-10" controlId="title">
-          <Form.Control className="inputTitle" type="text" placeholder="Book Title" />
-        </Form.Group>
+      <div className="add-book-form">
+        <div className="title mb-3 mr-10">
+          <div className="inputTitle" type="text" placeholder="Book Title" />
+        </div>
 
-        <Form.Select aria-label="Category">
+        <div aria-label="Category">
           <option>Category</option>
           <option value="Fantasy">Fantasy</option>
           <option value="Science Fiction">Science Fiction</option>
           <option value="Mystery">Mystery</option>
           <option value="Romance">Romance</option>
           <option value="Horror">Horror</option>
-        </Form.Select>
-        <Button className="addBookBtn" variant="primary" type="submit">
+        </div>
+        <button className="addBookBtn" variant="primary" type="submit">
           ADD BOOK
-        </Button>
-      </Form>
+        </button>
+      </div>
     </div>
   </div>
 );
